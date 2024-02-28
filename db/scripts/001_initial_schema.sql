ac60e826da161bac3db1c144c522a2ad396bde5f
SilentHacks <therealsilenthacks@gmail.com>
2024-02-28 19:34:15 +0000

    Update initial DB schema

--- db/scripts/001_initial_schema.sql
+++ db/scripts/001_initial_schema.sql
@@ -2,8 +2,75 @@
 
 CREATE TABLE IF NOT EXISTS movies (
     id INT PRIMARY KEY,
-    title TEXT NOT NULL
+    title TEXT NOT NULL,
+    imdb_id INT NOT NULL,
+    tmdb_id INT NOT NULL,
+    release_date DATE NOT NULL,
+    runtime INT NOT NULL,
+    tagline TEXT,
+    overview TEXT,
+    poster_path TEXT,
+    backdrop_path TEXT,
+    budget INT,
+    revenue INT,
+    status SMALLINT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO movies (id, title) VALUES
-(1, 'The Shawshank Redemption');
+CREATE TABLE IF NOT EXISTS genres (
+    id INT PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS movie_genres (
+    movie_id INT NOT NULL,
+    genre_id INT NOT NULL,
+    PRIMARY KEY (movie_id, genre_id),
+    FOREIGN KEY (movie_id) REFERENCES movies (id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id)
+);
+
+-- Contains the list of people involved in the movie (e.g. actors, directors, etc.)
+CREATE TABLE IF NOT EXISTS people (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    profile_path TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Contains the list of roles (e.g. actor, director, etc.)
+CREATE TABLE roles (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL
+);
+
+-- Contains the list of people involved in the movie and their roles (optionally, their character name in the movie)
+CREATE TABLE movie_people (
+    movie_id INT NOT NULL,
+    person_id INT NOT NULL,
+    role_id INT NOT NULL,
+    character_name TEXT,
+    PRIMARY KEY (movie_id, person_id, role_id),
+    FOREIGN KEY (movie_id) REFERENCES movies (id),
+    FOREIGN KEY (person_id) REFERENCES people (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+CREATE TABLE users (
+    id TEXT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE user_ratings (
+    user_id TEXT NOT NULL,
+    movie_id INT NOT NULL,
+    rating DECIMAL(2, 1) NOT NULL CHECK (rating >= 0 AND rating <= 5),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, movie_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (movie_id) REFERENCES movies (id)
+);
