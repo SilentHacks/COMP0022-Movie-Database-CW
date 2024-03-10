c832eef159c294a244ececc239450d5a7f6affe4
SilentHacks <therealsilenthacks@gmail.com>
2024-03-10 18:39:46 +0000

    Include movie_people.order in primary key

--- db/scripts/005_role_order.sql
+++ db/scripts/005_role_order.sql
@@ -1,4 +1,6 @@
 ALTER TABLE movie_people
     ADD COLUMN "order" INTEGER DEFAULT 0 NOT NULL;
-
-CREATE INDEX movie_people_order_idx ON movie_people ("order");
+ALTER TABLE movie_people
+    DROP CONSTRAINT movie_people_pkey;
+ALTER TABLE movie_people
+    ADD PRIMARY KEY (movie_id, person_id, role, "order");
